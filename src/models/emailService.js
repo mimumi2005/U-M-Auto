4045440b1cbb3849a65b9b60636129809d96644b
Mim <janiskrisjanis.g@gmail.com>
2025-05-15 15:14:18 +0300

    More email fixes

--- src/models/emailService.js
+++ src/models/emailService.js
@@ -17,7 +17,7 @@ export const sendAppointmentReminder = async (appointment, readableDate) => {
 
     const emailText = `Dear ${appointment.Username},\n\n` +
         `You have an appointment scheduled in one hour at ${readableDate}. ` +
-        `Please be sure to arrive at least 5 minutes before your scheduled time to ensure everything runs smoothly.\n\n` +
+        `\nPlease be sure to arrive at least 5 minutes before your scheduled time to ensure everything runs smoothly.\n\n` +
         `Appointment:\n${appointment.ProjectInfo}\n\n` +
         `Thank you for choosing our services! We look forward to seeing you soon.\n\n` +
         `Best regards,\nU&M Auto`;
@@ -28,7 +28,7 @@ export const sendAppointmentStatusUpdateAlert = async (appointment, newStatus) =
 
     const emailText = `Dear ${appointment.Username},\n\n` +
         `Your appointments status has been updated to: ${newStatus}. ` +
-        `For appointment of\n\n` +
+        `\nFor appointment of\n\n` +
         `\n${appointment.ProjectInfo}\n\n` +
         `Thank you for choosing our services! We hope you keep using our services.\n\n` +
         `Best regards,\nU&M Auto`;
@@ -39,7 +39,7 @@ export const sendAppointmentDateUpdateAlert = async (appointment, newEndDate) =>
 
     const emailText = `Dear ${appointment.Username},\n\n` +
         `Your appointments has been delayed to: ${newEndDate}. ` +
-        `For appointment of\n\n` +
+        `\nFor appointment of\n\n` +
         `\n${appointment.ProjectInfo}\n\n` +
         `Sorry for the inconveniance.\n\n` +
         `Best regards,\nU&M Auto`;
